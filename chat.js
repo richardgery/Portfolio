4e49aa44fe771e2ef997a1870b591423bde626c6
kancilcakep99 <kancilcakep99@gmail.com>
2022-11-23 20:29:06 +0700

    feat: add chatbox crisp in js file

---
+++ chat.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "066455ff-cb54-490c-8b2f-b8bc38fd120b";
+(function () {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
